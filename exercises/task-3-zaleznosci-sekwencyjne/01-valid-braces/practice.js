7fac3fc0e9ed1ad37d22391348964aa1cd809449
jacekdudziec <dudziecjacek@gmail.com>
2026-07-06 15:55:25 +0200

    Clear practice

--- exercises/task-3-zaleznosci-sekwencyjne/01-valid-braces/practice.js
+++ exercises/task-3-zaleznosci-sekwencyjne/01-valid-braces/practice.js
@@ -1,16 +1,1 @@
-export function validBraces(s) {
-  const pairs = { ")": "(", "]": "[", "}": "{" };
-  const stack = [];
-
-  for (const ch of s) {
-    if (ch === "(" || ch === "[" || ch === "{") {
-      stack.push(ch);
-    } else if (ch === ")" || ch === "]" || ch === "}") {
-      if (stack.pop() !== pairs[ch]) {
-        return false;
-      }
-    }
-  }
-
-  return stack.length === 0;
-}
+export function validBraces(s) {}
